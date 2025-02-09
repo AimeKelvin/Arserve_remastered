826a11e8905d5519566944820db7f6b78b2df0c3
Shimwa Aime Kelvin <158560776+AimeKelvin@users.noreply.github.com>
2025-02-09 12:54:45 +0200

    arserve going cookin

---
+++ src/components/blocks/Hero.tsx
@@ -0,0 +1,8 @@
+import { HeroGeometric } from "@/components/ui/shape-landing-hero"
+
+
+export default function Hero() {
+    return <HeroGeometric badge="Arserve Company"
+            title1 = "Let Us"
+            title2 = "Do It For You" />
+}
