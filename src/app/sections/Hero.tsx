2374fd8203010d24d1d8ce9ef99fb43ab6631e37
Shimwa Aime Kelvin <158560776+AimeKelvin@users.noreply.github.com>
2025-02-09 10:40:10 +0200

    restarting with next15

--- src/app/sections/Hero.tsx
+++
@@ -1,9 +0,0 @@
-import { HeroGeometric } from "@/components/ui/shape-landing-hero"
-
-
-export default function Hero() {
-    return <HeroGeometric badge="Arserve Company"
-            title1 = "Let Us"
-            title2 = "Do It For You" />
-}
-
